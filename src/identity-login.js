367af6a331b02a211ad424a6f0ce02837f4c4c13
Offirmo <offirmo.net@gmail.com>
2018-08-23 15:22:38 +1000

    TLDW :scream_cat:

--- src/identity-login.js
+++ src/identity-login.js
@@ -1,14 +1,16 @@
 // copied from https://www.netlify.com/docs/functions/
 
 exports.handler = function(event, context, callback) {
+	const all_the_things = JSON.stringify({
+		context,
+		event,
+		env: process.env,
+	}, null, 2)
 
-	console.log('hello')
-	console.info('hello')
-	console.warn('hello')
-	console.error('hello')
+	console.log('identity-login', all_the_things)
 
 	callback(null, {
 		statusCode: 200,
-		body: "Hello, World"
+		body: 'OK',
 	})
 }
