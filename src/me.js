809caa8249e86bc1a838d20be1efd4dd31cc2ac7
Offirmo <offirmo.net@gmail.com>
2018-08-23 16:11:44 +1000

    TLDW :scream_cat:

--- src/me.js
+++ src/me.js
@@ -4,7 +4,7 @@ exports.handler = function(event, context, callback) {
 	const { identity, user: {
 		email,
 		app_metadata: { provider, roles },
-		user_metadata: { avatar_url, full_url },
+		user_metadata: { avatar_url, full_name },
 	}} = context.clientContext;
 
 	const all_the_things = JSON.stringify({
@@ -12,7 +12,7 @@ exports.handler = function(event, context, callback) {
 		provider,
 		roles,
 		avatar_url,
-		full_url,
+		full_name,
 	}, null, 2)
 
 	console.log(all_the_things)
